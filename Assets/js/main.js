be1e20469743265fe41d6a4d3a2116e1b40a8420
ValMgz <valou32720@gmail.com>
2020-02-28 01:28:33 +0100

    fix h1 disapear

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -20,7 +20,7 @@ page.addEventListener('scroll', function() {
   }
   else if(page.scrollTop > window.innerHeight*4.5){
     document.getElementById('current-position').classList.add('invert-text')
-    document.getElementById('title').classList.add('invert-text')
+    document.getElementById('title').style.visibility = 'hidden';
     rightLinks[0].classList.add('invert-bouton')
 
     for(i=1;i<rightLinks.length;i++){
@@ -34,6 +34,7 @@ page.addEventListener('scroll', function() {
   else{
     document.getElementById('current-position').classList.remove('invert-text')
     document.getElementById('title').classList.remove('invert-text')
+    document.getElementById('title').style.visibility = 'visible';
     rightLinks[0].classList.remove('invert-bouton')
 
     for(i=0;i<dots.length;i++){
@@ -71,42 +72,36 @@ function UpdateScroll(){
     case 0:
       document.getElementById('current-position').innerHTML = "ACCUEIL";
       dots[0].classList.add('selected');
-      document.getElementById('title').style.visibility = 'visible';
 
       break;
 
     case window.innerHeight:
       document.getElementById('current-position').innerHTML = "LE CONCEPT";
       dots[1].classList.add('selected');
-      document.getElementById('title').style.visibility = 'visible';
 
       break;
 
     case window.innerHeight *2:
       document.getElementById('current-position').innerHTML = "NOTRE SOLUTION";
       dots[2].classList.add('selected');
-      document.getElementById('title').style.visibility = 'visible';
 
       break;
 
     case window.innerHeight*3:
       document.getElementById('current-position').innerHTML = "POURQUOI NOUS";
       dots[3].classList.add('selected');
-      document.getElementById('title').style.visibility = 'visible';
 
       break;
 
     case window.innerHeight*4:
       document.getElementById('current-position').innerHTML = "POURQUOI NOUS";
       dots[4].classList.add('selected');
-      document.getElementById('title').style.visibility = 'visible';
 
       break;
 
     case window.innerHeight*5:
       document.getElementById('current-position').innerHTML = "NOS JEUX";
       dots[5].classList.add('selected');
-      document.getElementById('title').style.visibility = 'hidden';
 
       break;
   
