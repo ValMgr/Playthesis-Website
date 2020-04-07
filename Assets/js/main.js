ac023acc63296508312a89adec8cd8b5cfbed8af
ValMgz <valou32720@gmail.com>
2020-04-07 22:30:24 +0200

    optimize intro video / responsive v1.0  / optimize main.js / add scroll.js & remove css snap scrolling

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,38 +1,49 @@
 /* 
  * Playthesis ©
- * Author: Valentin Magry - 2020
- * MMI Bordeaux
+ * Team: Playthesis 2020
+ * Developer: Valentin Magry
+ * 
+ * MMI Bordeaux - IUT Bordeaux Montaigne
  */
 
-var page = document.getElementById('content');
 
-page.addEventListener('scroll', function() {
+
+$(function() {
+  $.scrollify({
+    section : "section",
+    easing: "easeOutExpo",
+    scrollSpeed: 1500,
+  });
+});
+
+
+
+
+window.addEventListener('scroll', function() {
+  UpdateNavbar();
   
-   UpdateNavbar();
-
 });
 
 function ScrollTo(section){
-
-  target = document.getElementById(section);
-  target.scrollIntoView({
-      behavior: 'smooth',
-      block: 'center'
-  });
+  $.scrollify.move(section);
 }
 
 function WhiteText(){
   $('#current-position').addClass('invert-text');
-  $('#title').show().addClass('invert-text');
-  $('#connect').addClass('invert-bouton');
+  $('.title').show().addClass('invert-text');
+  $('.connect:eq(0)').addClass('invert-bouton');
   $('.dots').addClass('invert-dots')
+  $('.nav-span').children().addClass('white-burger')
+
 }
 
 function BlackText(){
   $('#current-position').removeClass('invert-text');
-  $('#title').show().removeClass('invert-text');
-  $('#connect').removeClass('invert-bouton');
+  $('.title').show().removeClass('invert-text');
+  $('.connect:eq(0)').removeClass('invert-bouton');
   $('.dots').removeClass('invert-dots')
+  $('.nav-span').children().removeClass('white-burger')
+
 }
 
 function UpdateNavbar(){
@@ -47,7 +58,7 @@ function UpdateNavbar(){
   // var height7 = height6 + $("#contact").height(); // #contact height
   var halfHeight = height0 / 2;
 
-  if(page.scrollTop < height0 - halfHeight){
+  if(window.scrollY < height0 - halfHeight){
      if($(".selected") != $(".dots")[0]){
           $(".selected").removeClass("selected");
           $(".dots:eq(0)").addClass("selected");
@@ -57,7 +68,7 @@ function UpdateNavbar(){
      }
   }
 
-  if(page.scrollTop > height0 -halfHeight && page.scrollTop < height1 -halfHeight){
+  if(window.scrollY > height0 -halfHeight && window.scrollY < height1 -halfHeight){
       if($(".selected") != $(".dots")[1]){
            $(".selected").removeClass("selected");
            $(".dots:eq(1)").addClass("selected");
@@ -67,7 +78,7 @@ function UpdateNavbar(){
       }
    }
 
-   if(page.scrollTop > height1 -halfHeight && page.scrollTop < height2 -halfHeight){
+   if(window.scrollY > height1 -halfHeight && window.scrollY < height2 -halfHeight){
       if($(".selected") != $(".dots")[2]){
            $(".selected").removeClass("selected");
            $(".dots:eq(2)").addClass("selected");
@@ -77,17 +88,23 @@ function UpdateNavbar(){
       }
    }
 
-   if(page.scrollTop > height2 -halfHeight && page.scrollTop < height3 -halfHeight){
+   if(window.scrollY > height2 -halfHeight && window.scrollY < height3 -halfHeight){
     if($(".selected") != $(".dots")[3]){
            $(".selected").removeClass("selected");
            $(".dots:eq(3)").addClass("selected");
            $("#current-position").html("POURQUOI NOUS");
            BlackText();
            $('#mute').hide();
+           if($(window).width() < 640){
+             $(".title").addClass("invert-text")
+           }
+           else{
+             $(".title").removeClass("invert-text")
+           }
       }
    }
  
-   if(page.scrollTop > height3 -halfHeight && page.scrollTop < height4 -halfHeight){
+   if(window.scrollY > height3 -halfHeight && window.scrollY < height4 -halfHeight){
     if($(".selected") != $(".dots")[4]){
            $(".selected").removeClass("selected");
            $(".dots:eq(4)").addClass("selected");
@@ -97,17 +114,17 @@ function UpdateNavbar(){
       }
    }
    
-   if(page.scrollTop > height4 -halfHeight && page.scrollTop < height5 -halfHeight){
+   if(window.scrollY > height4 -halfHeight && window.scrollY < height5 -halfHeight){
     if($(".selected") != $(".dots")[5]){
            $(".selected").removeClass("selected");
            $(".dots:eq(5)").addClass("selected");
            $("#current-position").html("NOS JEUX");
            WhiteText();
-           $('#title').hide();
+           $('.title').hide();
       }
    }
    
-   if(page.scrollTop > height5 -halfHeight && page.scrollTop < height6 -halfHeight){
+   if(window.scrollY > height5 -halfHeight && window.scrollY < height6 -halfHeight){
     if($(".selected") != $(".dots")[6]){
            $(".selected").removeClass("selected");
            $(".dots:eq(6)").addClass("selected");
@@ -117,7 +134,7 @@ function UpdateNavbar(){
       }
    }
    
-   if(page.scrollTop > height6 -halfHeight){
+   if(window.scrollY > height6 -halfHeight){
     if($(".selected") != $(".dots")[7]){
            $(".selected").removeClass("selected");
            $(".dots:eq(7)").addClass("selected");
@@ -130,15 +147,12 @@ function UpdateNavbar(){
 
 
 function muteSound(){
-  var audio = document.getElementById('myVideo');
-  var btn = document.getElementById('mute-btn');
-
-  if(audio.muted == false){
-    audio.muted = true;
-    btn.src = 'Assets/pictures/icons/mute.png';
+  if($('#video').prop('muted') == false){
+    $('#video').prop('muted', true);
+    $('#mute-btn').attr('src', 'Assets/pictures/icons/mute.png');
   }
   else{
-    audio.muted = false;
-    btn.src = 'Assets/pictures/icons/speaker.png';
+    $('#video').prop('muted', false);
+    $('#mute-btn').attr('src', 'Assets/pictures/icons/speaker.png');
   }
 }
